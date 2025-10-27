e9071cce06024e2f41b57c790a6cf979d9dc72d8
MonishaSrivastava <msrivastava@madeira.org>
2025-10-27 09:06:31 -0400

    Update pa2.py

--- pa2.py
+++ pa2.py
@@ -16,7 +16,7 @@ You may not necessarily have to edit the main function.
 
 #Need to ask for help because code is not adding to my score even though I have put the answer in correctly
 
-Score_file = "score_history.txt" #Python makes this file so it can put your scores in it
+Score_file = "score_history.txt" #Python makes this file so it can put your saved score + usernames 
 
 
 def play_quiz(filename):
@@ -25,14 +25,14 @@ def play_quiz(filename):
     
     print("Starting the flashcard quiz! Type 'quit' to stop early.\n")
  
-    score = 0 #start score
+    score = 0 #inital score
 
-    for term, definition in flashcards:
-        print(f"Question: {term}")
+    for term, definition in flashcards: #for loop goes through each flashcard one by one
+        print(f"Question: {term}") #Shows question with the variable defined as {term}
         answer = input("What is the answer? ").strip().lower()
         if answer.lower() == "quit":
             break
-        if answer.lower() == definition:
+        if answer.lower() == definition: #Shows definiton with the variable defined as definition
             print("Correct!\n")
             score += 1
         else:
@@ -46,12 +46,12 @@ def play_quiz(filename):
 
 def load_flashcards(filename):
     flashcards = []
-    with open(filename, "r") as f: #read function = r
+    with open(filename, "r") as f: #read function = r; python opens file in read mode
         for line in f:
-            if "-" in line:
+            if "-" in line: #only processes lines that contain a -
                 term, definiton = line.split("-", 1) #split() function, + setting the maxsplit parameter to 1, will return a list with 2 elements. https://www.w3schools.com/python/ref_string_split.asp
-                flashcards.append((term, definiton)) #reads then adds flashcard but i think i did something wrong in the parantheses
-    return flashcards
+                flashcards.append((term, definiton)) 
+    return flashcards #returns the list of flashcards
 
 
 def add_scores(username, score):
@@ -96,27 +96,27 @@ def main():
     while game_on:
         print("welcome to the review game")
         
-        while first_choice not in e_options:
-            for item in initial_choices:
+        while first_choice not in e_options: #while not exiting (runs because their first choice is an empty string (== ""), then because they haven't typed 'exit')
+            for item in initial_choices: #print out play, see history, and exit
                 print(f"- {item}")
             first_choice = input("What would you like to do?\n> ").lower().strip()
-            if first_choice in p_options:
+            if first_choice in p_options: #playing the game
                 quiz_fn = input("What is the name of your file?\n> ").lower().strip()
-                quiz_ext = input("Is it a .txt or .csv file?\n> ").lower().strip()
-                while quiz_ext not in file_types:
+                quiz_ext = input("Is it a .txt or .csv file?\n> ").lower().strip() 
+                while quiz_ext not in file_types: #if it is not a csv or txt then it will raise an error
                     print_error()
                     print("Your choices are:")
-                    for item in file_types:
+                    for item in file_types: #prints out txt and csv
                         print(f"- {item}")
-                    quiz_ext = input("Is it a .txt or .csv file?\n> ").lower().strip()
-                if quiz_ext in [".csv","csv"]:
+                    quiz_ext = input("Is it a .txt or .csv file?\n> ").lower().strip() #file types that are applicable
+                if quiz_ext in [".csv","csv"]: #comma seperated value file
                     file_url = quiz_fn+".csv"
                 else:
                     file_url = quiz_fn+".txt"
                 play_quiz(file_url)
-            elif first_choice in h_options:
+            elif first_choice in h_options: #looking at previous scores
                 show_scores()
-            elif first_choice in e_options:
+            elif first_choice in e_options: #exiting
                 game_on = False
             else:
                 print_error()
